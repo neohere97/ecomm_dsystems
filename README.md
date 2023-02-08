587695a0ceaf6ae170aa26aa38a837db6151dcbb
chinmay_shalawadi <shalawadi.chinmay@gmail.com>
2023-02-07 18:35:45 -0700

    Initial files created

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ecomm_dsystems
-This is an implementation of E-Commerce APIs in GoLang and Socket based communication. 
+This is an implementation of E-Commerce APIs 
+
+in GoLang and Socket based communication. 
+
+
+This is also a submission for Assignment 1 of ECEN 5673
+
---
+++ customer_database.go
@@ -0,0 +1,9 @@
+/***************************************************************************
+ * Distributed Systems Assignment 1
+ * Author: Chinmay Shalawadi
+ * Institution: University of Colorado Boulder
+ * Mail id: chsh1552@colorado.edu
+ * References: lecture slides & Golang documentation
+ ***************************************************************************/
+
+ 
